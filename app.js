14165ca25fa62434e9563603b9be4e4c251e8cbd
Michael Bamford <michael@michaelbamford.com>
2020-07-03 13:31:13 +1000

    Add Mongoose connection string

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const postRouter = require('./routes/posts_routes');
 
 const port = 3000;
 const app = express();
+
+const dbConn = 'mongodb://localhost/blog_app';
+
+mongoose.connect(dbConn,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    },
+    (err) => {
+        if (err) {
+            console.log('Error connecting to database', err);
+        } else {
+            console.log('Connect to database!');
+        }
+    }
+);
 
 app.use(cors());
 app.use(bodyParser.json());
